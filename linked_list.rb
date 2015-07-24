5756d97c9f6b92ac6a8b8b73885c920844e26456
Faisal <chaudhary.fsl@gmail.com>
2015-07-24 17:41:38 +0000

    linked list1

--- linked_list.rb
+++ linked_list.rb
@@ -5,4 +5,23 @@ class LinkedListNode
     @value = value
     @next_node = next_node
   end
+
 end
+
+
+def print_values(list_node)
+  print "#{list_node.value} --> "
+  if list_node.next_node.nil?
+    print "nil\n"
+    return
+  else
+    print_values(list_node.next_node)
+  end
+end
+
+node1 = LinkedListNode.new(37)
+node2 = LinkedListNode.new(99, node1)
+node3 = LinkedListNode.new(12, node2)
+
+
+print_values(node3)
