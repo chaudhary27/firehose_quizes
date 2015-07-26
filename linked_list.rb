725bf466e35adead97147b5a639f360128d48594
Faisal <chaudhary.fsl@gmail.com>
2015-07-26 19:55:18 +0000

    btree implementation from Tim's Blog

--- linked_list.rb
+++ linked_list.rb
@@ -1,4 +1,5 @@
 
+# Build a Reverse LinkedList WITHOUT using Arryas
 class LinkedListNode
   attr_accessor :value, :next_node
  
@@ -56,9 +57,11 @@ end
 
 def reverse_list(list)
     # ADD CODE HERE
+    #list = Sack.pop
 
     while list
         # ADD CODE HERE
+        # While list what?
         list = list.next_node
     end
 
