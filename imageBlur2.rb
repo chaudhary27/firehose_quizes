740d4bab7991e14590d680b346a2c7b6f2950324
Faisal <chaudhary.fsl@gmail.com>
2015-08-16 19:47:22 +0000

    complete imageblur2

--- imageBlur2.rb
+++ imageBlur2.rb
@@ -1,5 +1,6 @@
 class Image
 	attr_accessor :pixels
+
 	def initialize(pixels)
 		@pixels = pixels
 	end
@@ -12,27 +13,44 @@ class Image
 	end
 =end
 
-	def transform
-		empty_arr = []
-		@pixels.each do |i|
-			if i == 1
-				empty_arr << i
-			else
+	def index_of_ones
+		@ones_indexes = Array.new
 
+		pixels.each_with_index do |row, row_index|
+			row.each_with_index do |num, num_index|
+				@ones_indexes.push(row_index, num_index) if num == 1
 			end
-			puts empty_arr
 		end
-		
+	end
+
+	def transform_pixels
+		@ones_indexes.each do |m, n|
+			m = @ones_indexes.shift
+			n = @ones_indexes.shift
+
+			pixels[m-1][n] = 1 if pixels[m-1][n] != nil
+			pixels[m+1][n] = 1 if pixels[m+1][n] != nil
+			pixels[m][n-1] = 1 if pixels[m][n-1] != nil
+			pixels[m][n+1] = 1 if pixels[m][n+1] != nil
+		end
+	end
+
+	def output_tranformed_pixels
+		pixels.each do |element|
+			puts element.join
+		end
 	end
 
 end
 
 
 image = Image.new([
-  [0, 0, 0, 0],
-  [0, 1, 0, 0],
-  [0, 0, 0, 1],
-  [0, 0, 0, 0]
-  ])
+			[0,0,0,0],
+			[0,1,0,0],
+			[0,0,0,1],
+			[0,0,0,0]
+		])
 
-puts image.transform
+image.index_of_ones
+image.transform_pixels
+image.output_tranformed_pixels
