a36ee13c58588d8b8aac21ba67923dafcc7bd353
Faisal <chaudhary.fsl@gmail.com>
2015-09-01 21:08:20 +0000

    imageBlur3

--- linkedlist2.rb
+++ linkedlist2.rb
@@ -13,12 +13,12 @@ end
 
 def print_values(list_node)
   print "#{list_node.value} --> "
-  if list_node.next_node.nil?
-    print "nil\n"
-    return
-  else
-    print_values(list_node.next_node)
-  end
+    if list_node.next_node.nil?
+      print "nil\n"
+      return
+    else
+      print_values(list_node.next_node)
+    end
 end
 
 node1 = LinkedListNode.new(37)
